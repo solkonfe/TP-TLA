21df6c6706f0a3e17fc4d71bad7073ae6bf98e77
Milagros Cornidez <67882015+mcornidez@users.noreply.github.com>
2022-04-20 23:57:11 -0300

    Update tester.sh

--- tester.sh
+++ tester.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-rm -rf src/logs
+rm -rf logs
 
-mkdir src/logs
-mkdir src/logs/testlogs
+mkdir logs
+mkdir logs/testlogs
 
 for (( c=1; c<=15; c++ ))
 do
-    touch src/logs/testlogs/test${c}log.txt
     cat src/tests/test${c} | bin/Compiler > logs/testlogs/test${c}log.txt
 done
