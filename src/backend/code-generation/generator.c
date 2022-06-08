a975302c1e7cd6cc92dca551bd0b5ff1800d5de6
sol <skonfederak@itba.edu.ar>
2022-06-08 11:22:32 -0300

    correccion y agregado de prints

--- src/backend/code-generation/generator.c
+++ src/backend/code-generation/generator.c
@@ -38,7 +38,9 @@ void printHTML(tWebExpr * result, char * text){
 		case expr_type.TITLEEXPR:
 			printTitle(current->expr);
 		case expr_type.IMGEXPR:
+			printImage(current->expr);
 		case expr_type.LINKEXPR:
+			printLink(current->expr);
 		case expr_type.TABLEEXPR:
 		case expr_type.DIVEXPR:
 		case expr_type.TEXTEXPR:
@@ -72,24 +74,85 @@ void printTitle(tTitle * title){
 					underlined = 1;
 					break;
 			}	
+			currAttr = currAttr->next;
 		}
 	}
 	printf(">");
-	if (bold == 1)
-		printf("</b>");
-	if (italic == 1)
-		printf("</i>");
-	if (underlined == 1)
-		printf("</u>");
-	
-	printf("%s", title->value);
-	
 	if (bold == 1)
 		printf("<b>");
 	if (italic == 1)
 		printf("<i>");
 	if (underlined == 1)
 		printf("<u>");
+	
+	printf("%s", title->value);
+	
+	if (bold == 1)
+		printf("</b>");
+	if (italic == 1)
+		printf("</i>");
+	if (underlined == 1)
+		printf("</u>");
 
 	printf("</title>");
+}
+
+
+void printImage(tImage* image){
+	printf("<img ");
+	printf("src=\"%s\" ", image->source); //habria que ver si ya lo estamos guardando con comillas
+	if(image->idref != NULL){
+		printf("id=\"%s\" ", image->idref);
+	}
+	if(image->altText != NULL){
+		printf("alt=\"%s\" ", image->altText);
+	}
+	printf("/img>");
+}
+
+void printLink(tLink * link){
+	printf("<link ");
+	printf("href=\"%s\"", link->ref); //fijarse lo de las comillas
+	int bold = 0, italic = 0, underlined = 0;
+	if(link->attrs != NULL){
+		tAttribute * current= link->attrs->first;
+		while(current != NULL){
+			switch (current->type){
+				case atr_type.COLORVAL:
+					printf("color=%s ", current->value);
+					break;
+				case atr_type.POSITIONVAL:
+					printf("position=%s ", current->value);
+					break;
+				case atr_type.BOLDVAL:
+					bold = 1;
+					break;
+				case atr_type.ITALICVAL:
+					italic = 1;
+					break;
+				case atr_type.UNDERLINEDVAL:
+					underlined = 1;
+					break;
+			}
+			current = current->next;
+		}
+	}
+	printf(">");
+	if (bold == 1)
+		printf("<b>");
+	if (italic == 1)
+		printf("<i>");
+	if (underlined == 1)
+		printf("<u>");
+	
+	printf("%s", link->text);
+	
+	if (bold == 1)
+		printf("</b>");
+	if (italic == 1)
+		printf("</i>");
+	if (underlined == 1)
+		printf("</u>");
+
+	printf("</link>");
 }
